187369b2c8b8689dc3c9d714e85f29e6b195e59c
Ariel Anders <aanders@pr2mm1.(none)>
2015-01-16 13:52:50 -0800

    changing Turtle bot agent

--- TurtleBotAgent.py
+++ TurtleBotAgent.py
@@ -12,28 +12,28 @@ class TurtleAgent(Agent):
     def __init__(self, name):
         self.turtle_ctrl = Waiter(name)
         self.num = name == "leonardo"
-    
-    def get_observation(self):
-        print "getting an observation"
-        return [0,0,0,0]
 
     def do_action(self, action):
 
         if action == ACTIONS.ROOM_1:
-            self.turtle_ctrl.goToRoom1()
+            obs = self.turtle_ctrl.goToRoom1()
 
         elif action == ACTIONS.ROOM_2:
-            self.turtle_ctrl.goToRoom2()
+            obs = self.turtle_ctrl.goToRoom2()
 
         elif action == ACTIONS.ROOM_3:
-            self.turtle_ctrl.goToRoom3()
+            obs = self.turtle_ctrl.goToRoom3()
 
         elif action == ACTIONS.KITCHEN:
-            self.turtle_ctrl.goToKitchen()
+            obs = self.turtle_ctrl.goToKitchen()
 
         elif action == ACTIONS.GET_DRINK:
-            self.turtle_ctrl.waitAndGetDrink()
-        
+            obs = self.turtle_ctrl.waitAndGetDrink()
+        else:
+            print "incorrect action selected: %s " % action
+            raise Exception
+        return obs
+
         ###XXX gabe add this function to cleaner_waiter
         """ 
         def waitAndGetDrink(self):
