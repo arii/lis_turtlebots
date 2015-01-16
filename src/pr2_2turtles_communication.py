a1cbda3390db75db19e3f4a36b94005c4458d355
Ariel Anders <aanders@pr2mm1.(none)>
2015-01-16 15:09:47 -0800

    formatting

--- src/pr2_2turtles_communication.py
+++ src/pr2_2turtles_communication.py
@@ -11,23 +11,7 @@ from threading import Thread
 ARIEL = not True
 if ARIEL:
   from pick_and_place import pick_and_place
-"""
-class pr2_observer:
-    def __init__(self, pr2):
-        self.pr2 = pr2
-        self.observation_server = SimpleServer(port=12340, threading=True)
-        self.t = Thread (target = self.observe_pr2)
-        self.t.start()
 
-    def observe_pr2(self):
-        prev_obs = self.pr2.observe()
-        while True:
-            new_obs = self.pr2.observe()
-            if new_obs != prev_obs:
-                self.observation_server.update_broadcast(new_obs)
-            rospy.sleep(1)
-
-"""
 class interface:
   START, PICKING, WAITING_TO_PLACE, PLACING= range(4)
   def __init__(self, init_state=None, debug=False):
@@ -49,19 +33,9 @@ class interface:
           self.state = self.START
       else:
           self.state = init_state
-      #XXX self.observer = pr2_observer(self)
 
       self.event_loop()
-  """  
-  def observe(self):
-        if self.state == self.WAITING_TO_PLACE:
-            observation = "waiting_for_turtlebot"
-        elif self.state == self.PLACING:
-            observation = "serving_turtlebot"
-        else:
-            observation = "not_serving"
-        return observation
-  """
+  
   def event_loop(self):
       while True:
           rospy.loginfo("starting event loop!  current state = %d" % self.state)
@@ -71,16 +45,18 @@ class interface:
                   pass
                   #raw_input("Hit enter to start...")
               self.send_msg_to_turtle("not_serving: picking")
-              # threaded self.pick_up() tells if done
               self.t = Thread (target = self.pick_up)
               self.t.start()
               self.wait_until_msg_is("can i come", "generic_turtle")
-              self.server.update_broadcast("serving_turtlebot: %s " % self.turtle_being_attended)
+              self.server.update_broadcast("serving_turtlebot: %s "\
+                    % self.turtle_being_attended)
               
           if self.state == self.WAITING_TO_PLACE:
-              self.send_msg_to_turtle("serving_turtlebot: come " + self.turtle_being_attended)
+              self.send_msg_to_turtle("serving_turtlebot: come "\
+                    + self.turtle_being_attended)
               rospy.loginfo("Waiting for " + self.turtle_being_attended)
-              self.wait_until_msg_is("turtle in place position", self.turtle_being_attended)
+              self.wait_until_msg_is("turtle in place position", \
+                    self.turtle_being_attended)
               self.state = self.WAITING_TO_PLACE
           
           if self.state == self.PLACING:
@@ -88,7 +64,8 @@ class interface:
               self.place()
               self.send_msg_to_turtle("serving_turtlebot: pr2 placed object")
               #raw_input("placed object on %s " % self.turtle_being_attended)
-              self.wait_until_msg_is("turtle left pr2", self.turtle_being_attended)
+              self.wait_until_msg_is("turtle left pr2",\
+                    self.turtle_being_attended)
               self.turtle_being_attended = None
               self.state = self.PICKING
 
@@ -99,7 +76,8 @@ class interface:
       msg = self.receive_msg_from(name)
       while msg != correct_msg:
           msg = self.receive_msg_from(name)
-      info =  "message received is %s from %s" % (msg, self.turtle_being_attended)
+      info =  "message received is %s from %s" \
+            % (msg, self.turtle_being_attended)
       self.robot_speak(info)
       
   def send_msg_to_turtle(self, msg):
