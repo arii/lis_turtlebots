30a9d27616892b10991ce43dff4ddaf6e577ccd2
ari <aanders@mit.edu>
2015-03-06 14:35:06 -0500

    with voice commands

--- src/button_detector.py
+++ src/button_detector.py
@@ -1,17 +1,21 @@
 import serial
+import os
 
 class detector:
     def __init__(self, com="/dev/ttyUSB0", baud=9600, timeout=5):
         self.ser = serial.Serial(com, baud, timeout=timeout)
+        pass
     
-    def detect_button_press(self):
+    def detect_button_press(self, text = None):
+        if text != None:
+            os.system("espeak '%s' &" % text)
         button_pressed = self.ser.readline()
         return len(button_pressed) > 0
    
 if __name__=="__main__":
     d = detector()
     while True:
-        print d.detect_button_press()
+        print d.detect_button_press("hit the brain to get a drink")
 
 
 
