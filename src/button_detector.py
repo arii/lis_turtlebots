4f895d48263416642ff9a42c56eba163ad1c0f48
ari <aanders@mit.edu>
2015-03-10 14:26:04 -0400

    added arduino button

--- src/button_detector.py
+++ src/button_detector.py
@@ -2,25 +2,27 @@ import serial
 import os
 
 class detector:
-    def __init__(self, com="/dev/ttyUSB0", baud=9600, timeout=5):
+    def __init__(self, com="/dev/ttyUSB1", baud=9600, timeout=5):
         self.ser = serial.Serial(com, baud, timeout=timeout)
-        pass
     
     def detect_button_press(self, text = None):
+        self.ser.flushInput()
         if text != None:
-            os.system("espeak '%s' &" % text)
+            say(text)
         button_pressed = self.ser.readline()
+        #print button_pressed
         return len(button_pressed) > 0
 def say (text):
-    os.system("espeak '%s' &" % text)
+    os.system("espeak -a 200 -s 100 '%s'   2>/dev/null & " % text)
+
 if __name__=="__main__":
     d = detector()
+    print "starting detector"
     while True:
+        print d.detect_button_press()
         if d.detect_button_press("hit the brain to get a drink"):
              say("I will serve you a drink")
         else:
             say("you get nothing!")
-
-
 
 
